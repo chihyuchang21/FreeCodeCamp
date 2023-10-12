0d01637ddbfbbdbd2a6a826c3900f8abe6353e14
Cheryl Chang <68939820+chihyuchang21@users.noreply.github.com>
2023-10-12 09:59:11 +0800

    Create Backend_Managing Packages with NPM.js

---
+++ Backend_Managing Packages with NPM.js
@@ -0,0 +1,19 @@
+{
+	"author": "ChihYu Chang",
+  "name": "fcc-learn-npm-package-json",
+  "description": "A project that does something awesome",
+  "keywords": [ "freecodecamp", "test"],
+  "license": "MIT",
+  "version": "1.2.0", 
+	"dependencies": {
+		"express": "^4.14.0"
+	},
+	"main": "server.js",
+	"scripts": {
+		"start": "node server.js"
+	},
+	"repository": {
+		"type": "git",
+		"url": "https://idontknow/todo.git"
+	}
+}
