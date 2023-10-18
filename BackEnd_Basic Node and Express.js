8958b8ba4caf925971fd1e87c22604cf07e801c2
Cheryl Chang <68939820+chihyuchang21@users.noreply.github.com>
2023-10-18 20:55:15 +0800

    Update BackEnd_Basic Node and Express.js
    Update recent progress

--- BackEnd_Basic Node and Express.js
+++ BackEnd_Basic Node and Express.js
@@ -13,10 +13,22 @@ app.get('/',(req,res) => {
 })
 */
 
-//#4
+/*#4
+當應用程式收到以 "/public" 開頭的HTTP請求時，Express框架會把該請求映射到伺服器上的目錄__dirname + "/public"，並且把相對應的靜態文件返回給客戶端
+*/
 app.use("/public", express.static(__dirname + "/public"))
 
-//#3
+
+/*#3
+Express應用程式的路由設定。當應用程式接收到根目錄（"/"）的GET請求時，它會回傳伺服器上"/views/index.html"這個檔案的內容
+*/
 app.get('/',(req,res) => {
   res.sendFile(__dirname + "/views/index.html");
+})
+
+/*#5
+Express應用程式的路由設定。當應用程式接收到路徑為"/json"的GET請求時，它會回傳一個JSON格式的回應
+*/
+app.get('/json',(req,res) => {
+  res.json({"message":"Hello json"})
 })
